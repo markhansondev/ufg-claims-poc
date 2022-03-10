5e1a9e7517581294a0eef5a0f8d887c0e8baa945
Zach Behrensmeyer <zbehrensmeyer@unitedfiregroup.com>
2022-03-10 15:56:19 -0600

    Initial commit

---
+++ Poc.Claims/Poc.Claims.Specs/InitialReserve.feature
@@ -0,0 +1,8 @@
+﻿Feature: InitialReserve
+	Anything that involves Initial Reserves
+
+Scenario: Set an initial reserve amount because UFG needs to set money aside to payout
+	Given an FNOL is ready to be completed
+	And the FNOL line liability amount $1000
+	When the claim is created from an FNOL
+	Then the intial reserve is set to $1000
---
+++ Poc.Claims/Poc.Claims.Specs/InitialReserveSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Poc.Claims.Specs
+{
+    [Binding]
+    public class InitialReserveSteps
+    {
+        [Given(@"an FNOL is ready to be completed")]
+        public void GivenAnFNOLIsReadyToBeCompleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the FNOL line liability amount \$(.*)")]
+        public void GivenTheFNOLLineLiabilityAmount(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the claim is created from an FNOL")]
+        public void WhenTheClaimIsCreatedFromAnFNOL()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the intial reserve is set to \$(.*)")]
+        public void ThenTheIntialReserveIsSetTo(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
