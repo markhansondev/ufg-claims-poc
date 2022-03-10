41cb63e70cc13ebb129d19aa7d3d49f79fef168d
Ty Trinh <trinhty20@gmail.com>
2022-03-10 17:03:12 -0600

    add ReserveContext

--- Poc.Claims/Poc.Claims.Specs/InitialReserveSteps.cs
+++ Poc.Claims/Poc.Claims.Specs/InitialReserveSteps.cs
@@ -6,24 +6,40 @@ namespace Poc.Claims.Specs
     [Binding]
     public class InitialReserveSteps
     {
+
+        private class ReserveContext
+        {
+            public Fnol Fnol { get; set; }
+
+        }
+
+        private ReserveContext _reserveContext;
+
+        [BeforeScenario]
+        public void SetupScenario()
+        {
+            _reserveContext = new ReserveContext();
+            _reserveContext.Fnol = new Fnol();
+        }
+
         [Given(@"an FNOL is ready to be completed")]
         public void GivenAnFNOLIsReadyToBeCompleted()
         {
-            ScenarioContext.Current.Pending();
+            _reserveContext.Fnol.IsReadyToBeCompleted = true;
         }
-        
+
         [Given(@"the FNOL line liability amount \$(.*)")]
         public void GivenTheFNOLLineLiabilityAmount(int p0)
         {
             ScenarioContext.Current.Pending();
         }
-        
+
         [When(@"the claim is created from an FNOL")]
         public void WhenTheClaimIsCreatedFromAnFNOL()
         {
             ScenarioContext.Current.Pending();
         }
-        
+
         [Then(@"the intial reserve is set to \$(.*)")]
         public void ThenTheIntialReserveIsSetTo(int p0)
         {
---
+++ Poc.Claims/Poc.Claims/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.Claims
+{
+    public class Class1
+    {
+        private string a;
+        public Class1()
+        {
+            this.a = "Hello World";
+        }
+
+        public string b { get; set; }
+    }
+}
