1e42c3ebd3e7d409691a740e6f5cdd0a721f3d96
Hanson <MHanson@unitedfiregroup.com>
2022-05-05 14:15:31 -0700

    start defining the total claim reserve

--- Poc.Claims/Poc.Claims/Line.cs
+++ Poc.Claims/Poc.Claims/Line.cs
@@ -1,5 +1,6 @@
 ﻿namespace Poc.Claims
 {
+    //todo: should the client NOT be able to create a line independent of a claim?
     public class Line
     {
         public decimal ReserveAmount { get; set; }
