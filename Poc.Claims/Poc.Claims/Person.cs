ba3f65cba2dac13352c2146d0251db510d3e21bd
Zach Behrensmeyer <zbehrensmeyer@unitedfiregroup.com>
2022-09-29 14:04:58 -0500

    Adding initial implementation of Person Object

---
+++ Poc.Claims/Poc.Claims/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.Claims
+{
+    class Person
+    {
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+        
+        string MiddleName { get; set; }
+
+        DateTime DOB { get; set; }
+
+        public Person(string firstName, string lastName, DateTime dob)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DOB = dob;
+        }
+    }
+}
