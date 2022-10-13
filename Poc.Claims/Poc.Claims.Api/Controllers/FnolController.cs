48d4e246f5cb1d89129d058c1762f4dea993d5dd
Hanson <MHanson@unitedfiregroup.com>
2022-10-13 14:16:01 -0500

    start an api and service layer

---
+++ Poc.Claims/Poc.Claims.Api/Controllers/FnolController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Poc.Claims.Services;
+
+namespace Poc.Claims.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FnolController : ControllerBase
+    {
+        private readonly ILogger<FnolController> _logger;
+
+        public FnolController(ILogger<FnolController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+
+        public FnolDto Get(int id)
+        {
+            return FnolService.GetFnol();
+        }
+    }
+}
---
+++ Poc.Claims/Poc.Claims.Services/FnolService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Poc.Claims.Services
+{
+    public class FnolService
+    {
+        public static FnolDto GetFnol() => new() { line_type = "line type 1", claimant_name = "claimant 1"};
+    }
+
+    public class FnolDto
+    {
+        public string line_type { get; set; }
+        public string claimant_name { get; set; }
+    }
+}
